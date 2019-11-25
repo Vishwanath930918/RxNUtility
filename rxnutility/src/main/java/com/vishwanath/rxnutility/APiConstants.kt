8357292f5c8071d621991d742130d68d32804259
Vishwa19940831 <vishwanath@emproto.com>
2019-11-25 11:58:18 +0530

    changes

--- rxnutility/src/main/java/com/vishwanath/rxnutility/APiConstants.kt
+++ rxnutility/src/main/java/com/vishwanath/rxnutility/APiConstants.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 
 class Constants
 {
-    companion object{
+    /*companion object{
          const val DOWNLOAD_URL:String="https://my-json-server.typicode.com"
          const val SCRIPT_STORAGE_PATH=""
          const val Bundle_STORAGE_PATH=""
-    }
+    }*/
 }
